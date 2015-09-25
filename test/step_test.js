22b5491c69997676d6c4f45089a22cc1c699f5c2
Torsten Link <torsten.link@db.com>
2015-09-25 17:24:00 +0200

    initinitial checkin

---
+++ test/step_test.js
@@ -0,0 +1,17 @@
+/*global describe, it*/
+/* jslint node: true, esnext: true */
+"use strict";
+
+const chai = require('chai');
+const assert = chai.assert;
+const expect = chai.expect;
+const should = chai.should();
+
+
+describe("Dummy", function () {
+
+
+	it('Dummy', function (done) {
+		done();
+	});
+});
