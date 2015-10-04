13a533949d540c9549787b4ad553e4ca6aa48042
Torsten Link <torsten.link@db.com>
2015-10-04 12:16:29 +0200

    fix: Message creation was faulty. The parameters given where not correctly used

---
+++ test/message_test.js
@@ -0,0 +1,76 @@
+/*global describe, it*/
+/* jslint node: true, esnext: true */
+"use strict";
+
+const chai = require('chai');
+const assert = chai.assert;
+const expect = chai.expect;
+const should = chai.should();
+
+const Endpoint = require('../lib/endpoint');
+const messageFactory = require('../lib/message');
+
+
+describe("Message", function () {
+
+
+	it('create empty message', function (done) {
+		const msg = messageFactory();
+
+		assert.deepEqual(msg, {
+			"header": {},
+			"hops": [],
+			"messageType": {
+				"name": 'default',
+				"description": 'The default message for normal work'
+			},
+			"payload": {}
+		});
+
+		done();
+	});
+
+	it('create message with header', function (done) {
+		const msg = messageFactory({
+			"my": "name"
+		});
+
+		assert.deepEqual(msg, {
+			"header": {
+				"my": "name"
+			},
+			"hops": [],
+			"messageType": {
+				"name": 'default',
+				"description": 'The default message for normal work'
+			},
+			"payload": {}
+		});
+
+		done();
+	});
+
+	it('create message from existing message', function (done) {
+		const msg = messageFactory({
+			"my": "name"
+		});
+
+		const msgNew = messageFactory(undefined, msg);
+
+		assert.deepEqual(msgNew, {
+			"header": {
+				"my": "name"
+			},
+			"hops": [],
+			"messageType": {
+				"name": 'default',
+				"description": 'The default message for normal work'
+			},
+			"payload": {}
+		});
+
+		done();
+	});
+
+
+});
