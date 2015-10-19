6a64dd9d54b19c89d3aa2a663f244dc12bd893b6
Markus Felten <Markus.Felten@gmx.de>
2015-10-19 20:20:00 +0200

    wip

--- index.js
+++ index.js
@@ -12,13 +12,6 @@ module.exports.Endpoint = endpoint.Endpoint;
 module.exports.createEndpoint = endpoint.createEndpoint;
 module.exports.message = message;
 
-// registers some steps usefull for debugging
-exports.registerWithManagerTest = function (manager) {
-	manager.registerStep(require('./lib/steps/stepConsoleLogger'));
-	manager.registerStep(require('./lib/steps/StepEventGenerator'));
-	manager.registerStep(require('./lib/steps/stepPassThrough'));
-};
-
 /*
  * Creates a step from its configuration data.
  * 1.) Consultes the manager (stepImplementations) to retrieve the Step
@@ -38,9 +31,8 @@ exports.createStep = function (manager, scopeReporter, data, name) {
 	}
 
 	if (Impl) {
-		return new Impl(manager, scopeReporter, name, data);
+		return Imp._create(manager, scopeReporter, name, data);
 	} else {
-		console.log(`not found: ${data.type}`);
 		scopeReporter.error('Step implementation not found', 'step', name, data.type);
 	}
 };
