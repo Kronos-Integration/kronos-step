6a0829e5a58da4a0664629908d9a08f61fdb224a
Torsten Link <torsten.link@db.com>
2015-11-10 10:08:13 +0100

    work on new api

--- index.js
+++ index.js
@@ -2,13 +2,9 @@
 "use strict";
 
 const step = require('./lib/step'),
-	endpoint = require('./lib/endpoint'),
-	managerMock = require('./test/manager-mock');
+	endpoint = require('./lib/endpoint');
 
 exports.Step = step.BaseStep;
 exports.ScopeDefinitions = require('./lib/scopeDefinitions');
 
 exports.createEndpoint = endpoint.createEndpoint;
-
-// Used for test. Each step nest needs a manager. This is a mockup used for testing steps
-exports.managerMock = managerMock;
