58a2a6455624738aef4daa55716d47b6d75cc6ad
Torsten Link <torsten.link@db.com>
2016-01-02 18:22:18 +0100

    test(endpoint): Add the createEndpoint function in index.js. Most used by tests

--- index.js
+++ index.js
@@ -2,9 +2,17 @@
 "use strict";
 
 const step = require('./lib/step');
+const endpoint = require('./lib/endpoint');
 
-
-exports.endpoint = require('./lib/endpoint');
+exports.endpoint = endpoint;
 
 exports.Step = step.BaseStep;
 exports.ScopeDefinitions = require('./lib/scopeDefinitions');
+
+exports.createEndpoint = function (name, definition, step) {
+	if (definition.in) {
+		return new endpoint.ReceiveEndpoint(name, step);
+	} else {
+		return new endpoint.SendEndpoint(name, step);
+	}
+};
