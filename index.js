ebc5be2948ef324dddde3a8acb2af7da1fff8b70
Markus Felten <Markus.Felten@gmx.de>
2016-03-10 01:29:48 +0100

    feat: remove some deprecations
    BREAKING CHANGE: delete endpoint exports - use kronos-endpoints module itself
    BREAKING CHANGE: delete createEndpoint(name,def,step) - use new XYZEndpoint()

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
 /* jslint node: true, esnext: true */
 "use strict";
 
-const step = require('./lib/step'),
-	guardedEndpoint = require('./lib/guardedendpoint');
-exports.guardedEndpoint = guardedEndpoint;
-exports.endpoint = require('kronos-endpoint');
-exports.Step = step.BaseStep;
-
-exports.createEndpoint = function (name, definition, step) {
-	console.log("deprecated use new directly");
-	if (definition.in) {
-		return new guardedEndpoint.GuardedReceiveEndpoint(name, step);
-	} else {
-		return new guardedEndpoint.GuardedSendEndpoint(name, step);
-	}
-};
+exports.guardedEndpoint = require('./lib/guardedendpoint');
+exports.Step = require('./lib/step').BaseStep;
