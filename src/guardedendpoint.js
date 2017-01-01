99d1571cfafd1ce98bffa4a47a4e16a7dfe647c7
Markus Felten <Markus.Felten@gmx.de>
2017-01-01 17:45:18 +0100

    feat: rewrite as ES6 class

---
+++ src/guardedendpoint.js
@@ -0,0 +1,45 @@
+/* jslint node: true, esnext: true */
+
+'use strict';
+
+const endpoint = require('kronos-endpoint');
+
+class GuardedReceiveEndpoint extends endpoint.ReceiveEndpoint {
+
+  /**
+   * connect two endpoints
+   */
+  connect(otherEndpoint) {
+    if (otherEndpoint instanceof endpoint.ReceiveEndpoint) {
+      throw new Error("Could not connect two 'ReceiveEndpoint's together");
+    }
+    otherEndpoint.connected = this;
+  }
+}
+
+class GuardedSendEndpoint extends endpoint.SendEndpoint {
+  receive(request, formerRequest) {
+    if (!this.isConnected) {
+      throw new Error(`The endpoint '${this.name}' in the Step '${this.owner.name}' has no connected endpoint`);
+    }
+
+    if (!this.connected.receive) {
+      throw new Error(
+        `The endpoint '${this.connected}' in the Step '${this.connected.owner.name}' has no receive function`);
+    }
+    return this.connected.receive(request, formerRequest);
+  }
+
+  /**
+   * connect two endpoints
+   */
+  connect(otherEndpoint) {
+    if (otherEndpoint instanceof endpoint.SendEndpoint) {
+      throw new Error("Could not connect two 'SendEndpoint' together");
+    }
+    this.connected = otherEndpoint;
+  }
+}
+
+exports.GuardedReceiveEndpoint = GuardedReceiveEndpoint;
+exports.GuardedSendEndpoint = GuardedSendEndpoint;
