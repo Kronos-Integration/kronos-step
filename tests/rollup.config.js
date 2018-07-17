9c49007185d71fb2bf9dfd3aed4e0a1f7badb99a
Markus Felten <Markus.Felten@gmx.de>
2018-07-17 17:43:54 +0200

    chore(package): documentation@^8.0.1 chore(package): nyc@^13.0.0 chore(package): add rollup-plugin-commonjs@^9.1.3 chore(package): add rollup-plugin-node-resolve@^3.3.0 chore(package): add rollup-plugin-istanbul@^2.0.1 chore(package): add rollup-plugin-executable@^1.2.0 chore(package): semantic-release@^15.8.0 docs(package): remove keyword null chore(rollup): import resolve,commonjs,executable chore(rollup): import resolve,commonjs,istanbul chore(rollup): add istanbul,resolve,commonjs

--- tests/rollup.config.js
+++ tests/rollup.config.js
@@ -1,3 +1,7 @@
+import istanbul from 'rollup-plugin-istanbul';
+
+import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
 import multiEntry from 'rollup-plugin-multi-entry';
 
 export default {
@@ -8,5 +12,7 @@ export default {
     sourcemap: true
   },
   external: ['ava', 'kronos-endpoint', 'kronos-service', 'loglevel-mixin'],
-  plugins: [multiEntry()]
+  plugins: [multiEntry(), istanbul({
+    exclude: ['tests/**/*-test.js']
+  }), resolve(), commonjs()]
 };
